513b776b3a52d917d2851c030de97c6a5ef2025c
Lizbeth Carbajal López <151394655+LizbethCL@users.noreply.github.com>
2026-05-29 14:40:52 -0600

    proyecto en curso

---
+++ hospital/includes/sidebar.php
@@ -0,0 +1,28 @@
+<aside class="sidebar">
+
+    <div class="logo">
+        🏥 MediControl
+    </div>
+
+    <nav>
+
+        <a href="/hospital/index.php">
+            <i class="fa-solid fa-house"></i>
+            Dashboard
+        </a>
+
+        <a href="/hospital/doctores/listar.php">
+            <i class="fa-solid fa-user-doctor"></i>
+            Doctores
+        </a>
+
+        <a href="/hospital/citas/listar.php">
+            <i class="fa-solid fa-calendar-days"></i>
+            Citas
+        </a>
+
+        
+
+    </nav>
+
+</aside>
